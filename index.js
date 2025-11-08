2ab83c20a08c4de4b014c990c18ff95bfa7dcc33
Cipherous777 <elan.mpr@gmail.com>
2025-11-07 14:47:44 -1200

    commited

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 
 //routing
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/home", (req, res) => {
+  res.redirect("home");
+});
 app.get("/register.portal", (req, res) => {
   res.render("register");
 });
@@ -16,7 +22,16 @@ app.get("/register.portal/student", (req, res) => {
   res.render("studentRegister");
 });
 app.get("/register.portal/teacher", (req, res) => {
-  res.render("studentRegister");
+  res.render("teacherRegister");
+});
+app.get("/register.portal/formCompleted", (req, res) => {
+  res.render("formCompleted.ejs");
+});
+app.get("/register.portal/codefill", (req, res) => {
+  res.render("codefill");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 // running app
 app.listen(PORT, () => {
